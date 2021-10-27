78b4ab3af2c5236455261fff904b0ea4d3261c82
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2021-10-27 10:59:28 -0300

    Request scoped

--- src/main/java/github/guisofiati/firstjsf/PersonBean.java
+++ src/main/java/github/guisofiati/firstjsf/PersonBean.java
@@ -1,35 +1,37 @@
 package github.guisofiati.firstjsf;
 
-import javax.faces.bean.ManagedBean;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@RequestScoped
 @ManagedBean(name = "personBean")
 public class PersonBean {
 	
 	private String name;
-	private String lastName;
-	private String fullName;
 	
-	public String showName() {
-		fullName = name + " " + lastName;
+	private List<String> names = new ArrayList<String>();
+	
+	public String addName() {
+		names.add(name);
 		return "";
 	}
 	
+	public List<String> getNames() {
+		return names;
+	}
+
+	public void setNames(List<String> names) {
+		this.names = names;
+	}
+
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	public String getFullName() {
-		return fullName;
-	}
-	public void setFullName(String fullName) {
-		this.fullName = fullName;
 	}
 }
